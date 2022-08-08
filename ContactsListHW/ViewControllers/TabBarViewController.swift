9e0615488c72d5379ba5319a9b99ce62bb8864d2
Alex Sobolev <31108@mail.ru>
2022-08-08 20:54:20 +0300

    Create TabBarVC

---
+++ ContactsListHW/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  ContactsListHW
+//
+//  Created by Александр Соболев on 08.08.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
